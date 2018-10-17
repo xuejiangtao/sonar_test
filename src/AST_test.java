5a80455000c56cfcc5eb6637901c4c030bab7164
xuejiangtao <779480193@qq.com>
2018-10-17 20:14:00 +0800

    add file

---
+++ src/AST_test.java
@@ -0,0 +1,10 @@
+public class Increment{
+	int b = 1;
+	String a = "HelloWorld!";
+	public int DoSomething(int b){
+		return b + 1;
+	}
+	private print(){
+		print a;
+	}
+}
--- src/test1.java
+++ src/test1.java
@@ -2,8 +2,9 @@
 import java.sql.Connection;
 
 class test {
-	public int printfun ()
+	public int printfun (int A, int B)
 		{
+			double temp1=java.lang.Double.valueOf(A+B);
 			int temp_aaa=2;
 			int a=1;
 			return 0;
